1c63f7fc12b372378df394288a3a0a554c6422e3
SidhantBhardwaj61 <sidhantbharadwaj11@gmail.com>
2025-10-25 13:25:45 +0530

    Complete passcode enterer

--- Assets/Scripts/UI/Passcode/Passcode.cs
+++ Assets/Scripts/UI/Passcode/Passcode.cs
@@ -15,22 +15,42 @@ public class Passcode : MonoBehaviour
     // the number which is entered on pressing a button
     public void SetNumber(int num)
     {
-        if (enteredPassword.text.Length <= 4)
+        if (enteredPassword.text.Length <= 3)
         {
             enteredPassword.text += num.ToString();
         }
     }
-    
+
     // check the entered password
     public void Enter()
     {
         if (enteredPassword.text == correctPassword)
         {
             enteredPassword.text = "CORRECT!";
+            Invoke(nameof(Close) , 1f);
         }
         else
         {
             enteredPassword.text = "INCORRECT!";
+            Invoke(nameof(Clear), 1f);
         }
+    }
+
+    public void Backspace()
+    {
+        if (enteredPassword.text.Length > 0)
+        {
+            enteredPassword.text = enteredPassword.text.Substring(0, enteredPassword.text.Length - 1);
+        }
+    }
+
+    public void Clear()
+    {
+        enteredPassword.text = "";
+    }
+    
+    public void Close()
+    {
+        this.gameObject.SetActive(false);
     }
 }
