26dfd96ea25bb30ba6a69a7d338b9809a028669f
SidhantBhardwaj61 <sidhantbharadwaj11@gmail.com>
2025-10-31 14:39:15 +0530

    Fixed door teleport script

--- Assets/Scripts/Door/DoorTeleport.cs
+++ Assets/Scripts/Door/DoorTeleport.cs
@@ -4,17 +4,19 @@ public class DoorTeleport : MonoBehaviour
 {
     [SerializeField] Transform destination;
     [SerializeField] GameObject player;
+    [SerializeField] float exteriorOffset = 3f;
+    [SerializeField] float interiorOffset = 1f;
     Vector2 teleportPos;
 
     void OnEnable()
     {
         if (this.gameObject.tag == "Interior")
         {
-            teleportPos = new Vector2(destination.position.x, destination.position.y - 1f);
+            teleportPos = new Vector2(destination.position.x, destination.position.y - exteriorOffset);
         }
         else if(this.gameObject.tag == "Exterior")
         {
-            teleportPos = new Vector2(destination.position.x, destination.position.y + 1f);
+            teleportPos = new Vector2(destination.position.x, destination.position.y + interiorOffset);
         }
     }
 
